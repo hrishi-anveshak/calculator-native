a40d44781bda80056397a612d55d9501eb699e02
Hrishikesh <hrishikesh.jadhav@anveshak.com>
2025-05-07 12:44:29 +0530

    added homescreen and styles

---
+++ screens/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {styles} from '../styles/styles';
+import {ScrollView, Text, View} from 'react-native';
+export default function Home() {
+  return (
+    <View>
+      <Text style={styles.text}>Hi</Text>
+    </View>
+  );
+}
---
+++ styles/styles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+export const styles = StyleSheet.create({
+    text:{
+        color:'#ffff',
+        fontSize:  20,
+    },
+    highlight: {
+      fontWeight: '700',
+    },
+    bg:{
+        backgroundColor: '#0000',
+    },
+  });
+
+  export default styles;
